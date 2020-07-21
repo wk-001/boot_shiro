2fae6d2b505104df63ae627dc79a7bcb700d0773
wk <123@qq.com>
2020-07-21 22:00:29 +0800

    必须存在Redis才能启动项目

--- version1-redis/src/main/java/com/wk/exception/GlobalExceptionHandler.java
+++ version1-redis/src/main/java/com/wk/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.wk.exception;
 import com.wk.common.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.ShiroException;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -23,10 +24,17 @@ public class GlobalExceptionHandler {
 	/**
 	 * shiro相关异常
 	 */
+
+	@ExceptionHandler(value = UnauthorizedException.class)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public void handleUnauthorizedException(Exception e) {
+		log.error("权限不足：{}", e.getMessage());
+	}
+	
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)	//返回状态码
 	@ExceptionHandler(ShiroException.class)		//要捕获的异常
 	public Result<String> handler(ShiroException e){
-		log.error("-------------shiro异常：{}-------------",e.getMessage());
+		log.error("shiro异常：{}",e.getMessage());
 		return Result.fail(401,e.getMessage(),null);
 	}
 
@@ -36,7 +44,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(value = IllegalArgumentException.class)
 	public Result<String> handler(IllegalArgumentException e) throws IOException {
-		log.error("-------------Assert异常：{}-------------",e.getMessage());
+		log.error("Assert异常：{}",e.getMessage());
 		return Result.fail(e.getMessage());
 	}
 
@@ -46,7 +54,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
 	public Result<String> handler(MethodArgumentNotValidException e) throws IOException {
-		log.error("-------------实体校验异常：{}-------------",e.getMessage());
+		log.error("实体校验异常：{}",e.getMessage());
 		BindingResult bindingResult = e.getBindingResult();
 		ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
 		return Result.fail(objectError.getDefaultMessage());
@@ -58,7 +66,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)	//返回状态码
 	@ExceptionHandler(RuntimeException.class)	//要捕获的异常
 	public Result<String> handler(RuntimeException e){
-		log.error("-------------运行时异常：{}-------------",e.getMessage());
+		log.error("运行时异常：{}",e.getMessage());
 		return Result.fail(e.getMessage());
 	}
 
